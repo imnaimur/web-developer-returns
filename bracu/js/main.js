3a8f058aab590ff0e79d30116a32002eea0cd1ef
Naimur <naimur58co@gmail.com>
2023-06-09 22:41:48 +0600

    js

--- bracu/js/main.js
+++ bracu/js/main.js
@@ -1,2 +1,24 @@
-let ages = 20;
-alert(`if your age is ${ages} +.`);
+const toDos = [
+    {
+        "id": 1,
+        "text": "Take the trash",
+        "status":true
+    },
+    {
+        "id": 2,
+        "text": "Meeting with boss",
+        "status":true
+    },
+    {
+        "id": 1,
+        "text": "Doctor appt",
+        "status":false
+    }
+]
+
+const statusCheck = toDos.filter(function(todo){
+    return todo.status == true;
+}).map(function(todo){
+    console.log( todo.text);
+})
+// console.log(statusCheck)
